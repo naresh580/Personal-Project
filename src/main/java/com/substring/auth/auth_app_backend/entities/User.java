5695817421eadc45c34cd22b33b8fe97bc43e9cf
naresh580 <nareshpandey580@gmail.com>
2026-05-12 00:36:12 +0530

    created entity classes

--- src/main/java/com/substring/auth/auth_app_backend/entities/User.java
+++ src/main/java/com/substring/auth/auth_app_backend/entities/User.java
@@ -1,13 +1,29 @@
 package com.substring.auth.auth_app_backend.entities;
 
+import jakarta.persistence.*;
+import lombok.*;
+
 import java.time.Instant;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.UUID;
 
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name="users")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "user_id")
     private UUID id;
+    @Column(name = "user_name", length = 300)
     private String name;
+    @Column(name = "user_email", unique = true,length = 500, nullable = false)
     private String email;
     private String password;
     private String image;
@@ -15,6 +31,24 @@ public class User {
     private Instant createdAt = Instant.now();
     private Instant updatedAt = Instant.now();
 
+    @Enumerated(EnumType.STRING)
     private Provider provider =Provider.LOCAL;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "user_roles",
+    joinColumns = @JoinColumn(name = "user_id"),
+    inverseJoinColumns = @JoinColumn(name = "role_id"))
     private Set<Role> roles = new HashSet<>();
+
+    @PrePersist
+    protected void onCreate(){
+        Instant now = Instant.now();
+        if(createdAt==null) createdAt = now;
+        updatedAt = now;
+    }
+
+    @PreUpdate
+    protected void onUpdate(){
+        updatedAt = Instant.now();
+    }
 }
